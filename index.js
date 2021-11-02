f543558153b78cea40b107e09d740013ce6d0a00
Behnam <behnam.khshab2003@gmaul.com>
2021-11-02 19:08:18 +0100

    add solution

---
+++ index.js
@@ -0,0 +1,24 @@
+const A= true
+if(A=== false)
+{
+  console.log("A is:",A);
+}
+const B= false
+if(B=== false)
+{
+  console.log("B is: ",B);
+}
+
+const Nr1=5
+const Nr2=10
+if(Nr1==5 && Nr2>=5)
+{
+  console.log("if statement is:",true);
+}
+
+const Nr3=6
+const Nr4=20
+if(Nr3!==6 && Nr4<30)
+{
+  console.log("if statement is:",true);
+}
